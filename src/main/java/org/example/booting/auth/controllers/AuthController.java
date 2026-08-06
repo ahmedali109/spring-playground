92423fc4fbe1a8290bfc6d92643b8317d30e5442
ahmedali109 <ahmedalinaguib33@gmail.com>
2026-08-06 19:45:10 +0300

    create Auth Controller

---
+++ src/main/java/org/example/booting/auth/controllers/AuthController.java
@@ -0,0 +1,68 @@
+package org.example.booting.auth.controllers;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.example.booting.auth.dto.request.LoginRequest;
+import org.example.booting.auth.dto.request.SignupRequest;
+import org.example.booting.auth.dto.response.SignupResponse;
+import org.example.booting.auth.dto.response.UserResponse;
+import org.example.booting.auth.service.AuthService;
+import org.example.booting.auth.user.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody SignupRequest signupRequest){
+        User user = authService.signup(signupRequest);
+
+        UserResponse userResponse = new UserResponse(
+                user.getId(),
+                user.getEmail()
+        );
+
+        SignupResponse response = new SignupResponse(
+                "User Created",
+                userResponse
+        );
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest, HttpServletResponse response){
+        String token = authService.login(loginRequest);
+        Cookie cookie = new Cookie("token", token);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(60 * 60 * 24);
+        response.addCookie(cookie);
+        return ResponseEntity.ok("Loggin in");
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletResponse response){
+
+        Cookie cookie = new Cookie("token", "");
+
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+
+        response.addCookie(cookie);
+
+        return ResponseEntity.status(HttpStatus.OK).body("User Logged out successfully");
+    }
+
+}

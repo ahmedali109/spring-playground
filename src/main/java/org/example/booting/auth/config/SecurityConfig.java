2e363fa6503c6cf86c03aba19167e09cbd666228
ahmedali109 <ahmedalinaguib33@gmail.com>
2026-08-08 16:47:01 +0300

    Update Security Configurations

--- src/main/java/org/example/booting/auth/config/SecurityConfig.java
+++ src/main/java/org/example/booting/auth/config/SecurityConfig.java
@@ -1,30 +1,38 @@
 package org.example.booting.auth.config;
 
+import lombok.RequiredArgsConstructor;
+import org.example.booting.auth.service.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtFilter;
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http
-                .csrf(csrf -> csrf.disable())
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+             http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
-                );
-
+                )
+                .addFilterBefore(jwtFilter , UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
-
 }
